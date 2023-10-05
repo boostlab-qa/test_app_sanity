1ea63d51385e5ff7fbbb45cae8d95728e77665c8
BBmgpark <110453550+BBmgpark@users.noreply.github.com>
2023-10-05 20:34:41 +0900

    수정 #1

--- Object Repository/00_data_delete/input_search.rs
+++ Object Repository/00_data_delete/input_search.rs
@@ -3,7 +3,7 @@
    <description>설정 > 애플리케이션 > 검색 > 입력</description>
    <name>input_search</name>
    <tag></tag>
-   <elementGuidId>435a6bc0-e3ab-41d7-9f04-88a76a8cb4ca</elementGuidId>
+   <elementGuidId>c3657f68-6816-4d90-b11c-3107c50bff02</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
@@ -12,15 +12,7 @@
       <name>resource-id</name>
       <type>Main</type>
       <value>com.android.settings:id/search_src_text</value>
-      <webElementGuid>34e21d7b-e66f-4470-a1bd-0c3e1f65ab2f</webElementGuid>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>text</name>
-      <type>Main</type>
-      <value>앱 검색</value>
-      <webElementGuid>8e291f5d-b742-49d0-b4e1-e37fbed1a2f3</webElementGuid>
+      <webElementGuid>b048e0d5-8426-412c-94da-226e7d180994</webElementGuid>
    </webElementProperties>
    <locator>//*[@resource-id = 'com.android.settings:id/search_src_text' and (@text = '앱 검색' or . = '앱 검색')]</locator>
    <locatorStrategy>XPATH</locatorStrategy>
